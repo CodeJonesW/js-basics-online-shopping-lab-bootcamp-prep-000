921f20ad0b9f4870bce615d157d705aa02d8ac28
Will Jones <willjones2010@gmail.com>
2019-03-05 20:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,9 +40,15 @@ return totalValue;
 }
 
 function removeFromCart(item) {
-cart[i].slice
+cart.slice()
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return `Sorry, we don't have a credit card on file for you.`;
+  }
+  if (cardNumber) {
+    cart = 0;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
 }
