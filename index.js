59baf122fde7b9461e575f231bfed1a7bf0a2039
Will Jones <willjones2010@gmail.com>
2019-03-05 19:46:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function viewCart() {
   }
  for (var i = 0; i < cart.length; i ++) {
    if (i === cart.length-1) {
-     newString += `and ${cart[i].itemName} at ${cart[i].itemPrice}.`;
+     newString += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
    } else {
-   newString += `${cart[i].itemName} at ${cart[i].itemPrice},`;
+   newString += `${cart[i].itemName} at $${cart[i].itemPrice},`;
    }
 }
 return newString;
